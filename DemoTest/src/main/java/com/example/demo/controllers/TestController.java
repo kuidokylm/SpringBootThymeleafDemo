6bbe286a7349045ab078eb8f161ce82768508cb6
kuido.kulm <kuido.kulm@W018.aktors.ee>
2018-10-15 09:37:07 +0300

    Lisatud test

---
+++ DemoTest/src/main/java/com/example/demo/controllers/TestController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+	
+	@GetMapping(value= {"/test"})
+	@ResponseBody
+    public String greeting() {
+        return "Hello World";
+    }
+
+}
--- DemoTest/src/test/java/com/example/demo/DemoTestApplicationTests.java
+++ DemoTest/src/test/java/com/example/demo/DemoTestApplicationTests.java
@@ -1,16 +1,33 @@
 package com.example.demo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.example.demo.controllers.TestController;
+
+//https://spring.io/guides/gs/testing-web/
+
+//The @SpringBootTest annotation tells Spring Boot to go and look for a main configuration class 
+//(one with @SpringBootApplication for instance), and use that to start a Spring application context
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest  
 public class DemoTestApplicationTests {
 
+
+	
+	@Autowired
+    private TestController controller;
+	
+	
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception {		
+		assertThat(controller).isNotNull();
 	}
 
 }
