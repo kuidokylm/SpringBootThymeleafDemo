7ed03fa50ae6ff1d9fed7be11870f83bc8983379
kuido.kulm <kuido@koodikaru.ee>
2018-12-31 11:15:16 +0200

    kommenaar lisatud kuidas Eclipse all käivitada

--- DemoTest/src/main/java/com/example/demo/controllers/DefaultController.java
+++ DemoTest/src/main/java/com/example/demo/controllers/DefaultController.java
@@ -1,6 +1,6 @@
 package com.example.demo.controllers;
 
-
+//Eclipse Run As Spring Boot App
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
