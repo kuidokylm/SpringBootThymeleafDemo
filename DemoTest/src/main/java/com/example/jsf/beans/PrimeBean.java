3e355c337a83f09b96a341523bc3e758dfeb113d
kuido.kulm <kuido@koodikaru.ee>
2019-01-04 14:19:22 +0200

    sorteeritav tabel

--- DemoTest/src/main/java/com/example/jsf/beans/PrimeBean.java
+++ DemoTest/src/main/java/com/example/jsf/beans/PrimeBean.java
@@ -1,10 +1,16 @@
 package com.example.jsf.beans;
 
 import java.io.Serializable;
+import java.util.List;
 
 //import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.inject.Named;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.demo.dto.Country;
+import com.example.repository.CountryJdbcRepository;
 
 
 //@ManagedBean(name = "primeBean", eager = true)  //see ei tööta
@@ -12,9 +18,15 @@ import javax.inject.Named;
 @SessionScoped  //@ViewScoped
 public class PrimeBean implements Serializable {
 
+	@Autowired
+	CountryJdbcRepository repo;
+	
 	private static final long serialVersionUID = 3860730229822198456L;
 	private String firstName="Kuido";
 	private String lastName="Külm";
+	
+	private List<Country> riigid;
+	
 	public String getFirstName() {
 		return firstName;
 	}
@@ -27,4 +39,14 @@ public class PrimeBean implements Serializable {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+	
+	public List<Country> getRiigid() {
+		if (this.riigid == null)
+		{
+			this.riigid=repo.findAllCountry();
+		}
+		return this.riigid;
+		//return repo.findAllCountry();
+	}
+	
 }
